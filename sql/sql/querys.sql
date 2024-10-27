636489e3d6c0ef829844113c3eb23b81e03cd186
unknown <Aramirez2022@udec.cl>
2024-10-26 21:20:51 -0300

    Actualización de las consultas

--- sql/sql/querys.sql
+++ sql/sql/querys.sql
@@ -1,16 +1,42 @@
-SELECT Estudiante.Nombre_Est, Estudiante.Apellido_Est, Asignatura.Nombre_Asig
-FROM Estudiante
-JOIN Cursa ON Estudiante.Rut_Est = Cursa.Rut_Est
-JOIN Asignatura ON Cursa.Cod_Asig = Asignatura.Cod_Asig;
+//TERCERA CONSULTA
+SELECT 
+    E.Nombre_Est, 
+    E.Apellido_Est, 
+    A.Nombre_Asig
+FROM 
+    Proyecto.Estudiante AS E
+JOIN 
+    Proyecto.Cursa AS C ON E.Rut_Est = C.Rut_Est
+JOIN 
+    Proyecto.Asignatura AS A ON C.Cod_Asig = A.Cod_Asig;
 
-SELECT Apoderado.Nombre_Ap, Apoderado.Direccion_Ap, Apoderado.Telefono_Ap, Estudiante.Nombre_Est, Estudiante.Apellido_Est, Presenta.Nota_Ev
-FROM Apoderado
-JOIN Estudiante ON Apoderado.Rut_Ap = Estudiante.Rut_Apoderado_Est
-JOIN Presenta ON Estudiante.Rut_Est = Presenta.Rut_Est
-WHERE Presenta.Nota_Ev > 6.0;
+//SEGUNDA CONSULTA
+SELECT 
+    A.Nombre_Ap, 
+    A.Direccion_Ap, 
+    A.Telefono_Ap, 
+    E.Nombre_Est, 
+    E.Apellido_Est, 
+    P.Nota_Ev
+FROM 
+    Proyecto.Apoderado AS A
+JOIN 
+    Proyecto.Estudiante AS E ON A.Rut_Ap = E.Rut_Apoderado_Est
+JOIN 
+    Proyecto.Presenta AS P ON E.Rut_Est = P.Rut_Est
+WHERE 
+    P.Nota_Ev > 6.0;
 
-SELECT Empleado.Nombre_Emp AS Profesor, Departamento.Nombre_Dep, Jefe.Nombre_Emp AS Jefe_Departamento
-FROM Empleado
-JOIN ParteDe ON Empleado.Rut_Emp = ParteDe.Rut_Emp
-JOIN Departamento ON ParteDe.ID_Dep = Departamento.ID_Dep
-JOIN Empleado AS Jefe ON Departamento.Rut_Jefe_Dep = Jefe.Rut_Emp;
+//TERCERA CONSULTA
+SELECT 
+    E.Nombre_Emp AS Profesor, 
+    D.Nombre_Dep, 
+    J.Nombre_Emp AS Jefe_Departamento
+FROM 
+    Proyecto.Empleado AS E
+JOIN 
+    Proyecto.ParteDe AS PD ON E.Rut_Emp = PD.Rut_Emp
+JOIN 
+    Proyecto.Departamento AS D ON PD.ID_Dep = D.ID_Dep
+JOIN 
+    Proyecto.Empleado AS J ON D.Rut_Jefe_Dep = J.Rut_Emp;
